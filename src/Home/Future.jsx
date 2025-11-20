64900def1497f433a9ce91b52a8708f85ec25610
bayazedhasan <bm.bayzid13@gmail.com>
2025-11-20 23:32:25 +0600

    fisrt commit

--- src/Home/Future.jsx
+++ src/Home/Future.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BsFillLightningChargeFill } from "react-icons/bs";
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+import { HiOutlineLightBulb } from "react-icons/hi";
+
 
 const Future = () => {
     return (
-        <div className='container mx-auto px-28 flex items-center gap-10'>
+        <div className='container mx-auto px-28 flex items-center gap-10 rele'>
+            
             <div className='flex items-center gap-6'>
                 <div>
                     <img className='rounded-tl-[70px] rounded-br-[70px]' src="/assets/content-img-1.png" alt="" />
@@ -26,12 +30,29 @@ const Future = () => {
                             Better Future</h1>
                     </div>
                     <div>
-                        <p className='p'>This includes offering personalized feedback, fostering a sense of community through discussion forums and group projects, and providing continuous support to address challenges and improve.</p>
+                        <p className='p pb-4'>This includes offering personalized feedback, fostering a sense of community through discussion forums and group projects, and providing continuous support to address challenges and improve.</p>
                     </div>
-                    <div>
-                        <div className='border rounded-full p-6  hover:bg-[#07A698] bg-[#E1F2FF]'>
-                            <img src="/assets/content-1.png" alt="" />
+                    <div className=''
+                    >
+                        <div className='flex items-center gap-4 border rounded-xl p-5 border-gray-100 mb-6 hover:shadow-2xl hover:scale-101 duration-300 shadow '>
+                            <div className='border rounded-full px-6 py-6 text-[#07A698] hover:text-white hover:bg-[#07A698] duration-500 bg-[#E1F2FF]'>
+                                <RiVerifiedBadgeFill size={42} />
+                            </div>
+                            <div>
+                                <h2 className='pc py-2 text-lg font-bold'>Master Certified Trainer</h2>
+                                <p className=' w-100 text-[#757978]'>This includes offering personalized feedback, fostering a sense of community through discussion.</p>
+                            </div>
                         </div>
+                        <div className='flex items-center gap-4 border rounded-xl p-5 border-gray-100 mb-6 hover:shadow-2xl hover:scale-101 duration-300 shadow'>
+                            <div className='border rounded-full px-6 py-6 text-[#07A698] hover:text-white hover:bg-[#07A698] duration-500 bg-[#E1F2FF]'>
+                                <HiOutlineLightBulb size={42} />
+                            </div>
+                            <div>
+                                <h2 className='pc py-2 text-lg font-bold'>Master Certified Trainer</h2>
+                                <p className=' w-100 text-[#757978]'>This includes offering personalized feedback, fostering a sense of community through discussion.</p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
